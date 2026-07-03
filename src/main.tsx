f2475bba41c428bd78c4c08a562547b5d9870614
miguelbaumann1981 <miguelmartinezm@outlook.es>
2026-07-03 13:08:11 +0200

    clean code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import App from './App.tsx'
 import { LeChatonAdminApp } from './LeChatonAdminApp.tsx';
 import { I18nProvider } from './i18n/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
     <I18nProvider>
       <LeChatonAdminApp />
     </I18nProvider>
